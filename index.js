076198d71949ee666e2da678f15d7e293a743cf2
Nora Vogt <noraevogt@gmail.com>
2018-10-31 14:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue) {
-  for (i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     array[i] = changeValue;
     if (i === stopValue) {
       break;
