232731617d0e45de2bf38e6357f92e62e25ddc58
Nora Vogt <noraevogt@gmail.com>
2018-10-31 14:19:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function breakOut(array, changeValue, stopValue) {
+  for (i = 0; i < array.length; i++) {
+    array[i] = changeValue;
+    if (i === stopValue) {
+      break; 
+    }
+    return array;
+  }
+}
