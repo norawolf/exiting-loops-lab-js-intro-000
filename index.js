0709c74e6b8b08c7d8ebb015a30c45519f7d232b
Nora Vogt <noraevogt@gmail.com>
2018-10-31 14:43:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
       return array[i]
+    } else {
+      return null;
     }
-  } else {
-    return null;
-  }
+  } 
 }
 
-//findBy(array, findFn) which looks for a value in array based on the return value of findFn.
-//Return null if the value isn't found.
+//findBy(array, findFn) which looks for a value in array based on the return value of findFn. 
+//Return null if the value isn't found. 
 //Hint: Check the test file to know the signature of the findFn and the type of its return value
